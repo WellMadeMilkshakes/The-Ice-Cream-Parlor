b2a74b304d4b6550ed293e9240f6a5191babc842
zechariah <myerszechariahj@gmail.com>
2021-08-19 19:45:27 -0400

    Main page changes artistic

--- pages/index.js
+++ pages/index.js
@@ -44,76 +44,86 @@ return (
   <div className="font-pacifico" id="about">
   
 {/* -------------------------------------------------------------------- */}
+    
+
     <div class="w-11/12 md:w-10/12 m-auto mt-10">
-      <div id="countdown" className="py-6 content-center my-8 p-8">
+      <div id="countdown" className="p-0 content-center my-8">
         <div className="flex flex-wrap lg:flex-nowrap justify-around items-center mt-8">
-          <div className="w-3/4 text-center items-center justify-center">
-            <h1 className="text-3xl md:text-5xl my-10 text-white font-indie text-blau">THE ICE CREAM PARLOR</h1>
-            <h2 className="text-3xl text-gray-200 ">IT'S HAPPENING!!! <br/><span className="">Be the first to mint your cone in ...</span></h2>
-            <div className="pl-8 md:pl-10 lg:pl-0 xl:pl-20">
-              <iframe className="block md:hidden" src="https://free.timeanddate.com/countdown/i7y0i6hp/n1170/cf12/cm0/cu4/ct2/cs1/ca0/co0/cr0/ss0/cacfff/cpcf00/pct/tcfff/fn3/fs200/szw256/szh108/iso2021-08-26T18:00:00" allowtransparency="true" frameborder="0" width="152" height="44"></iframe>
-              <iframe className="hidden md:block" src="https://free.timeanddate.com/countdown/i7y0i6hp/n1170/cf12/cm0/cu4/ct2/cs1/ca0/co0/cr0/ss0/cacfff/cpcf00/pct/tcfff/fn3/fs250/szw576/szh243/iso2021-08-26T18:00:00" allowtransparency="true" frameborder="0" width="428" height="121"></iframe>
+          <div className="items-center justify-center">
+          <h1 className="text-center text-5xl md:text-8xl my-10 text-white font-indie text-blau">THE ICE CREAM PARLOR</h1>
+
+            <div className="grid gap-4 sm:grid-cols-0 md:grid-cols-5">
+              <div className="col-span-3 shadow-lg rounded-lg bg-gray-700 p-6">
+                <h2 className="text-3xl text-gray-200 mt-3">IT'S HAPPENING!!! <br/><span className="">Be the first to mint your cone in ...</span></h2>
+                <div className="pl-auto">
+                  <iframe className="block md:hidden" src="https://free.timeanddate.com/countdown/i7y0i6hp/n1170/cf12/cm0/cu4/ct2/cs1/ca0/co0/cr0/ss0/cacfff/cpcf00/pct/tcfff/fn3/fs200/szw256/szh108/iso2021-08-26T18:00:00" allowtransparency="true" frameborder="0" width="152" height="44"></iframe>
+                  <iframe className="hidden md:block" src="https://free.timeanddate.com/countdown/i7y0i6hp/n1170/cf12/cm0/cu4/ct2/cs1/ca0/co0/cr0/ss0/cacfff/cpcf00/pct/tcfff/fn3/fs250/szw576/szh243/iso2021-08-26T18:00:00" allowtransparency="true" frameborder="0" width="428" height="121"></iframe>
+                </div>
+                <p className="text-2xl text-gray-200 my-6">
+                  <strong>LAUNCH DATE:</strong> Tuesday, 24 August 2021, 6pm EST. <br />‍
+                  <span className="text-gray-200 text-2xl"><strong>TOTAL SUPPLY:</strong> 8888 Cones.<br/><strong>PRICE:</strong> 0.04 ETH each.</span>
+                </p>
+                {/* <iframe src="https://free.timeanddate.com/countdown/i7vcex5b/n107/cf11/cm0/cu4/ct0/cs1/ca2/co0/cr0/ss0/cacfff/cpcfff/pct/tcfff/fn3/fs100/szw448/szh189/iso2021-06-29T21:00:00" allowtransparency="true" frameBorder="0" width="425" height="25"></iframe> */}
+              </div>
+
+              <img className="col-span-2 rounded-xl shadow-2xl" src="/images/NewGIF.gif"></img>
+
             </div>
-            <p className="text-2xl text-gray-200 my-6">
-              <strong>LAUNCH DATE:</strong> Tuesday, 24 August 2021, 6pm EST. <br />‍
-              <span className="text-gray-200 text-2xl"><strong>TOTAL SUPPLY:</strong> 8888 Cones.<br/><strong>PRICE:</strong> 0.04 ETH each.</span>
-            </p>
-            {/* <iframe src="https://free.timeanddate.com/countdown/i7vcex5b/n107/cf11/cm0/cu4/ct0/cs1/ca2/co0/cr0/ss0/cacfff/cpcfff/pct/tcfff/fn3/fs100/szw448/szh189/iso2021-06-29T21:00:00" allowtransparency="true" frameBorder="0" width="425" height="25"></iframe> */}
 
           </div>
         </div>
-
-        <div className="flex flex-col items-center font-indie">
-          {/* href="/mint" */}
-          <a href="" className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau rounded-lg text-white hover:text-black py-2 px-6 ">Mint!</a>
-        </div> 
+        
+          <div className="flex flex-col items-center font-indie mt-10">
+            {/* href="/mint" */}
+            <a href="" className="shadow-lg Poppitandfinchsans text-4xl border-6 bg-blau rounded-lg text-white hover:text-black py-2 px-6 ">Mint!</a>
+          </div>
       </div>
     </div>
 
 {/* -------------------------------------------------------------------- */}
 
-    <div id="roadmap" className="content-center">
+    <div id="roadmap" className="content-center my-12">
       <img id="roadmap-img" className="pb-8 hidden md:block" src="/images/Roadmap.png"/>
       <img id="roadmap-img" className="pb-8 block md:hidden" src="/images/RoadmapMobile.png"/>
       <div class="px-10 md:px-40 bg-gray-700 shadow-lg p-4 rounded-t-3xl">
 
-      <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4">OUR VISION</h2>
-      <p className="text-2xl text-white text-center my-6"> Simple. Our vision is to push the industry in the NFT space.  
-      </p>
-      <p className="text-2xl text-white text-center my-6">No more cheap projects that were done in a week, no more rug pulls where a product is promised and poorly delivered. NO MORE MONEY GRABS! Our goal is to push everyone to do better than before on delivering a good product that has purpose!</p>
+        <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4">OUR VISION</h2>
+        <p className="text-2xl text-white text-center my-6"> Simple. Our vision is to push the industry in the NFT space.  
+        </p>
+        <p className="text-2xl text-white text-center my-6">No more cheap projects that were done in a week, no more rug pulls where a product is promised and poorly delivered. NO MORE MONEY GRABS! Our goal is to push everyone to do better than before on delivering a good product that has purpose!</p>
 
-      <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4">THE DIFFERENCE</h2>
-      <p className="text-xl text-white text-center my-6 font-indie"><span className="text-3xl">O</span>VER <span className="text-3xl text-blau"> ONE MILLION </span> COMBINATIONS<span className="text-3xl">!</span></p>
-      <p className="text-2xl text-white text-center my-6">Some of our cones have costumes. Each costume is randomized in and of itself. For example the pirates alone have more than 500 variations! With 20+ costumes, multiple flavors, cone types, expressions, backgrounds and toppings each piece will truly be unique and look good too! ;)</p>
-      <p className="text-xl text-white text-center my-6 font-indie"><span className="text-3xl">S</span>UPER RARE <span className="text-3xl text-blau"> DOUBLE CONES</span><span className="text-3xl">!</span></p>
-      <p className="text-2xl text-white text-center my-6">We are pushing the industry by having an 8% chance when you mint you will get <span className="underline">TWO cones in one photo</span> instead of just one. Double cone photos coming to the ART GALLERY soon!</p>
+        <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4">THE DIFFERENCE</h2>
+        <p className="text-xl text-white text-center my-6 font-indie"><span className="text-3xl">O</span>VER <span className="text-3xl text-blau"> ONE MILLION </span> COMBINATIONS<span className="text-3xl">!</span></p>
+        <p className="text-2xl text-white text-center my-6">Some of our cones have costumes. Each costume is randomized in and of itself. For example the pirates alone have more than 500 variations! With 20+ costumes, multiple flavors, cone types, expressions, backgrounds and toppings each piece will truly be unique and look good too! ;)</p>
+        <p className="text-xl text-white text-center my-6 font-indie"><span className="text-3xl">S</span>UPER RARE <span className="text-3xl text-blau"> DOUBLE CONES</span><span className="text-3xl">!</span></p>
+        <p className="text-2xl text-white text-center my-6">We are pushing the industry by having an 8% chance when you mint you will get <span className="underline">TWO cones in one photo</span> instead of just one. Double cone photos coming to the ART GALLERY soon!</p>
 
-      <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4">GIVING BACK</h2>
+        <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4">GIVING BACK</h2>
 
-      <p className="text-2xl text-white my-6">Fullfilling our roadmap will allow us to...</p>
-      <ul className="">
-        <li className="pl-6 text-2xl text-white my-6">1) Donate 10 ETH to a charity chosen by our community!</li>
-        <li className="pl-6 text-2xl text-white my-6">2) Give 8 giveaways of .8888 ETH to our amazing community!</li>
-        <li className="pl-6 text-2xl text-white my-6">3) Launch our next project and really start to push the boundaries of NFTs!</li>  
-        <li className="pl-6 text-2xl text-white my-6">4) We have set aside 88 NFTs just for you guys! #giveAways</li>  
-      </ul>
+        <p className="text-2xl text-white my-6">Fullfilling our roadmap will allow us to...</p>
+        <ul className="">
+          <li className="pl-6 text-2xl text-white my-6">1) Donate 10 ETH to a charity chosen by our community!</li>
+          <li className="pl-6 text-2xl text-white my-6">2) Give 8 giveaways of .8888 ETH to our amazing community!</li>
+          <li className="pl-6 text-2xl text-white my-6">3) Launch our next project and really start to push the boundaries of NFTs!</li>  
+          <li className="pl-6 text-2xl text-white my-6">4) We have set aside 88 NFTs just for you guys! #giveAways</li>  
+        </ul>
 
-      <p className="text-2xl text-white my-6"> In addition, we have been, and continue to make <span className="font-bold underline"> high quality content for the NFT community!</span>
-      </p>
+        <p className="text-2xl text-white my-6"> In addition, we have been, and continue to make <span className="font-bold underline"> high quality content for the NFT community!</span>
+        </p>
 
-      <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4 mt-10">WHY BACK US?</h2>
-      <p className="text-2xl text-white text-center my-6">NO MORE UGLY ART!</p>
-      <p className="text-2xl text-white text-center my-6"> After entering the NFT space we came to a conclusion. There are simply too many lazy projects! Not to mention, the art just looks too… random! We are sick and tired of clashing triple cheetah print robots… no artist would make that shit! We decided to make art that is intelligently designed. Art that is still randomized but will look good no matter how you put it! With this vision we are looking to raise the bar in the NFT space FOREVER!
-      </p>
+        <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4 mt-10">WHY BACK US?</h2>
+        <p className="text-2xl text-white text-center my-6">NO MORE UGLY ART!</p>
+        <p className="text-2xl text-white text-center my-6"> After entering the NFT space we came to a conclusion. There are simply too many lazy projects! Not to mention, the art just looks too… random! We are sick and tired of clashing triple cheetah print robots… no artist would make that shit! We decided to make art that is intelligently designed. Art that is still randomized but will look good no matter how you put it! With this vision we are looking to raise the bar in the NFT space FOREVER!
+        </p>
 
-      <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4">OUR GAURENTEE</h2>
-      <p className="text-2xl text-white text-center my-6">We are in this for the long run!
-      </p>
-      <p className="text-2xl text-white text-center my-6">Why is the drop so soon? Our plan is to do a slow drop. Why over excite the market and jack up gas prices for no reason?
-      </p>
+        <h2 className="text-3xl md:text-6xl text-center text-gray-200 font-frederick my-4">OUR GAURENTEE</h2>
+        <p className="text-2xl text-white text-center my-6">We are in this for the long run!
+        </p>
+        <p className="text-2xl text-white text-center my-6">Why is the drop so soon? Our plan is to do a slow drop. Why over excite the market and jack up gas prices for no reason?
+        </p>
 
+      </div>
     </div>
-  </div>
       
 {/* -------------------------------------------------------------------- */}
 
@@ -158,9 +168,21 @@ return (
             <div className="description col-span-2">
               <p class="text-xl font-medium">wellMadeMilkshakes</p>
               <p class="">Project Development</p>
+              <p class="">Front End Developer</p>
               <a class="text-lg text-blau font-indie" href="https://twitter.com/wellMadeShakes">@wellMadeShakes</a>
             </div>
           </div>  
+
+          <div className="grid gap-4 grid-cols-3 p-2 items-center">
+            <div className="image">
+              <img className="rounded-full shadow-lg" src="/images/profiles/Mummy.png"></img>
+            </div>
+            <div className="description col-span-2">
+              <p class="text-xl font-medium">TheScoopy1</p>
+              <p class="">Marketing</p>
+              <a class="text-lg text-blau font-indie" href="https://twitter.com/TheScoopy1">@TheScoopy1</a>
+            </div>
+          </div> 
 
         </div>
 
@@ -196,6 +218,17 @@ return (
               <p class="text-xl font-medium">MetaverseMogul</p>
               <p class="">Developer</p>
               <a class="text-lg text-blau font-indie" href="https://twitter.com/MetaverseMogul">@MetaverseMogul</a>
+            </div>
+          </div>
+
+          <div className="grid gap-4 grid-cols-3 p-2 items-center">
+            <div className="image">
+              <img className="rounded-full shadow-lg" src="/images/profiles/Shark.png"></img>
+            </div>
+            <div className="description col-span-2">
+              <p class="text-xl font-medium">Annonymous</p>
+              <p class="">Developer</p>
+              {/* <a class="text-lg text-blau font-indie" href="https://twitter.com/MetaverseMogul">@MetaverseMogul</a> */}
             </div>
           </div>
 
