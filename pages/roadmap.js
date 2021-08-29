8f762487dff97150c03665b7f82ae42d799aff04
zechariah <myerszechariahj@gmail.com>
2021-08-29 19:21:23 -0400

    Roadmap update

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -24,7 +24,25 @@ return (
     <meta name="twitter:image" content="https://theicecreamparlor/images/profile/Hippie.png" key="twimage" />
   </Head>
 
+  <div className="" id="Art">
+    <img id="MainWelcomePhoto" src="/images/Welcome2.png"></img>
+  </div>
 
+  <div className="bg-gray-700 w-full shadow-md font-indie">
+
+    <div className="flex items-center justify-between">
+      <nav className="ml-4 md:ml-10 flex flex-wrap flex-row text-xl md:text-4xl">
+        {/* <a href="/mint" className="text-4xl text-white hover:text-black m-6">MINT!</a> */}
+        <a href="#roadmap" className="text-white hover:text-black m-6">Roadmap</a>
+        <a href="#team" className=" text-white hover:text-black m-6">Team</a>
+        <a href="#Gallery" className=" hidden md:block text-white hover:text-black m-6">Gallery</a>
+        <a href="https://twitter.com/IceCreamNFT" target="_blank" className=" hover:text-white m-6 text-blau">twitter</a>
+        <a href="https://discord.com/channels/876818020633313290/876819378623086624" target="_blank" className=" hover:text-white m-6 text-blau">discord</a>
+      </nav>
+      {/* <a href="/" className=""><img src="/images/Ice_Cream_gif.gif" width="240" alt="" className="logo-image" /></a> */}
+    </div>
+
+  </div>
 </div>
 )
 }
